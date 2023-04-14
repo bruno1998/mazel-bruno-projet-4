b2f44816edf2fa36f482a214f3f3e5f4a7dee07d
bruno.mazel <bruno.mazel@adelya.com>
2023-04-14 13:50:21 +0200

    fix: fixed a but were the timer could send negative amount of time spend in the parking log

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,8 +10,8 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        int inHour = ticket.getInTime().getTime();
+        int outHour = ticket.getOutTime().getTime();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
         int duration = outHour - inHour;
